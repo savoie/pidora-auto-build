77b97c71dcbe20346fb6c43da4bbdfb2f66e904f
Celine <ceya931@gmail.com>
2014-08-15 13:42:31 -0400

    Use rpm -i

--- kernelupdate.bash
+++ kernelupdate.bash
@@ -10,44 +10,32 @@ target_email='ceya931@gmail.com' # email to send reports to
 mkdir -p ~/pidora-build/kernel
 cd ~/pidora-build/kernel
 
-# Retrieves latest commit from github
+# Configures rpmbuild directories
+rpmdev-setuptree # Builds directory tree at ~/rpmbuild if not exists
+rpmdev-wipetree # Erases all files from rpmdev tree
+sources="$HOME/rpmbuild/SOURCES" # Location of SOURCES
+specs="$HOME/rpmbuild/SPECS" # Location of SPECS
+srpms="$HOME/rpmbuild/SRPMS" # Location of SRPMS
+
+# Retrieves and extracts latest commit from github
 commit_long=$(git ls-remote https://github.com/raspberrypi/linux.git | sed -n "s/\(.*\)\s.*$branch/\1/p") # 40-character unique commmit ID
 commit_short=$(echo $commit_long | cut -c1-7) # First 7 characters of full commit ID
-
-: <<'END'
-
-if [ -e $commit_long ]
-then
-	exit
-else
-	find . -type d -name "raspberrypi-linux*" | xargs rm -rf
-	rm *
-fi
-
-END
-
 wget https://github.com/raspberrypi/linux/tarball/$commit_long # Downloads a tarball of the latest commit with filename $commit_long
+cp $commit_long $sources # Copies the new tarball to ~/rpmbuild/SOURCES
+tar -xf $commit_long # Extracts tarball into directory called raspberrypi-linux-$commit_short
 
-# Retrieves and extracts latest raspberry pi kernel from koji
+# Retrieves and extracts latest kernel build from koji
 latest_kernel=$(armv6-koji latest-build $build_tag raspberrypi-kernel | sed -n "s/\([^ ]*\)\s*$build_tag\s*.*/\1/p") # Gets name of latest kernel build from koji
 armv6-koji download-build --arch=src $latest_kernel # Downloads source rpm file (filename $latest_kernel.src.rpm)
-rpm2cpio $latest_kernel.src.rpm | cpio -imd
-
-# Gets information about the older build (to correctly name files extracted from the old src rpm)
-old_version=$(sed -n 's/Version:\s*\(.*\)/\1/p' raspberrypi-kernel.spec)
-old_commit=$(sed -n 's/%global commit_short\s*\(.*\)/\1/p' raspberrypi-kernel.spec)
-
-# Extracts tarball into directory called raspberrypi-linux-$commit_short
-tar -xf $commit_long
+rpm -i $latest_kernel.src.rpm # Extracts src.rpm to ~/rpmbuild/SOURCES and ~/rpmbuild/SPECS
 
 # Determines current version number
 sublevel=$(cat raspberrypi-linux-$commit_short/Makefile | sed -n 's/SUBLEVEL = \([0-9]*\)/\1/p') # Retrieves version sublevel (z in version number x.y.z) from Makefile
 version=$(echo $branch | sed -n "s/.*-\([0-9]*\.[0-9]*\.\).*/\1$sublevel/p") # Combines branch and sublevel numbers to get version number
-#version="$(echo $branch | grep -o -m1 '[0-9]*\.[0-9]*\.')$sublevel" # Combines branch and sublevel numbers to get version number
 
 # Updates the configuration file
-bunzip2 pidora-config-$old_version-$old_commit.bz2
-mv pidora-config-$old_version-$old_commit ./raspberrypi-linux-$commit_short/.config
+bunzip2 $sources/pidora-config*
+mv $sources/pidora-config* ./raspberrypi-linux-$commit_short/.config
 cd raspberrypi-linux-$commit_short
 
 # - Makes a new configuration file based on the settings declared in the old config file
@@ -55,38 +43,29 @@ cd raspberrypi-linux-$commit_short
 # - Stores changed settings in variable 'changes'
 changes=$(yes '' | make oldconfig ARCH=arm | grep '(NEW)')
 
-cp .config ../pidora-config-$version-$commit_short
-cd ..
-bzip2 pidora-config-$version-$commit_short
+cp .config $sources/pidora-config-$version-$commit_short
+bzip2 $sources/pidora-config-$version-$commit_short
 
 # Updates the spec file
 sed -i -e "s/%global commit_date\s*.*/%global commit_date  $date/"\
 	-e "s/%global commit_short\s*.*/%global commit_short $commit_short/"\
 	-e "s/%global commit_long\s*.*/%global commit_long  $commit_long/"\
 	-e "s/Version:\s*.*/Version:        $version/"\
-	-e "s/Release:\s*[0-9]*/Release:        00/" raspberrypi-kernel.spec # Set to 00 so bumpspec will set it to 1
-rpmdev-bumpspec -c 'updated to latest commit' -u 'pidora-auto-build' raspberrypi-kernel.spec
+	-e "s/Release:\s*[0-9]*/Release:        00/" $specs/raspberrypi-kernel.spec # Set to 00 so bumpspec will set it to 1
+rpmdev-bumpspec -c 'updated to latest commit' -u 'pidora-auto-build' $specs/raspberrypi-kernel.spec
 
-# Sets up the document tree and moves files
-rpmdev-setuptree
-rpmdev-wipetree
-cp first32k.bin.bz2 ~/rpmbuild/SOURCES
-cp $commit_long ~/rpmbuild/SOURCES
-cp pidora-config-$version-$commit_short.bz2 ~/rpmbuild/SOURCES
-cp raspberrypi-kernel.spec ~/rpmbuild/SPECS
+# Builds the rpm into ~/rpmbuild/SRPMS
+rpmbuild -bs $specs/raspberrypi-kernel.spec
 
-# Builds the rpm and uploads to koji
-cd ~/rpmbuild/SPECS
-rpmbuild -bs raspberrypi-kernel.spec
-cd ~/rpmbuild/SRPMS
+# Uploads src rpm to koji
 echo 'Uploading to koji...'
-file_name="raspberrypi-kernel-${version}-1.${date}git${commit_short}.rpfr20.src.rpm"
-task_id=$(armv6-koji build --scratch --wait $auto_tag $file_name | sed -n 's/Created task: \([0-9]*\)/\1/p')
-#task_id=$(armv6-koji build --scratch --wait $auto_tag $file_name | grep 'Created task:' | grep -o '[0-9]*')
+file_name="$srpms/raspberrypi-kernel-${version}-1.${date}git${commit_short}.rpfr20.src.rpm"
+#task_id=$(armv6-koji build --scratch --wait $auto_tag $file_name | sed -n 's/Created task: \([0-9]*\)/\1/p')
+task_id=$(armv6-koji build --scratch --wait $auto_tag $file_name | grep 'Created task:' | grep -o '[0-9]*')
 
 # Reports success/failure
-task_status=$(armv6-koji taskinfo $task_id | sed -n 's/State: \(.*\)/\1/p')
-#task_status=$(armv6-koji taskinfo $task_id | grep 'State:' | cut -d ' ' -f2)
+#task_status=$(armv6-koji taskinfo $task_id | sed -n 's/State: \(.*\)/\1/p')
+task_status=$(armv6-koji taskinfo $task_id | grep 'State:' | cut -d ' ' -f2)
 echo "$task_status (ID: $task_id)"
 
 if [ "$task_status" = "closed" ]
